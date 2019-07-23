9a638173176a7c9ac859a7e9312eb79ab0243f80
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-23 15:39:23 +0600

    ৩৩.২ - থিমে বুটস্ট্র্যাপিং এবং অ্যাসেটস ইমপোর্টিং
    ৩৩.২ - থিমে বুটস্ট্র্যাপিং এবং অ্যাসেটস ইমপোর্টিং

--- app/public/wp-content/themes/simpleshop/functions.php
+++ app/public/wp-content/themes/simpleshop/functions.php
@@ -1,165 +1,117 @@
 <?php
 
+if (!function_exists('simpleshop_setup')):
 
+    function simpleshop_setup()
+{
 
+        load_theme_textdomain('simpleshop', get_template_directory() . '/languages');
 
+        add_theme_support('automatic-feed-links');
 
+        add_theme_support('title-tag');
 
-if ( ! function_exists( 'simpleshop_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function simpleshop_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Twenty Nineteen, use a find and replace
-		 * to change 'simpleshop' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'simpleshop', get_template_directory() . '/languages' );
+        add_theme_support('post-thumbnails');
+        set_post_thumbnail_size(1568, 9999);
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+        register_nav_menus(
+            array(
+                'menu-1' => __('Primary', 'simpleshop'),
+                'footer' => __('Footer Menu', 'simpleshop'),
+                'social' => __('Social Links Menu', 'simpleshop'),
+            )
+        );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support(
+            'html5',
+            array(
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+            )
+        );
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-		set_post_thumbnail_size( 1568, 9999 );
+        add_theme_support(
+            'custom-logo',
+            array(
+                'height'      => 190,
+                'width'       => 190,
+                'flex-width'  => false,
+                'flex-height' => false,
+            )
+        );
 
-		// This theme uses wp_nav_menu() in two locations.
-		register_nav_menus(
-			array(
-				'menu-1' => __( 'Primary', 'simpleshop' ),
-				'footer' => __( 'Footer Menu', 'simpleshop' ),
-				'social' => __( 'Social Links Menu', 'simpleshop' ),
-			)
-		);
+        add_theme_support('custom-background', apply_filters('simpleshop_custom_background_args', array(
+            'default-color' => 'ffffff',
+            'default-image' => '',
+        )));
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-			)
-		);
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 190,
-				'width'       => 190,
-				'flex-width'  => false,
-				'flex-height' => false,
-			)
-		);
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		// Add support for Block Styles.
-		add_theme_support( 'wp-block-styles' );
-
-		// Add support for full and wide align images.
-		add_theme_support( 'align-wide' );
-
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style-editor.css' );
-
-		// Add custom editor font sizes.
-		add_theme_support(
-			'editor-font-sizes',
-			array(
-				array(
-					'name'      => __( 'Small', 'simpleshop' ),
-					'shortName' => __( 'S', 'simpleshop' ),
-					'size'      => 19.5,
-					'slug'      => 'small',
-				),
-				array(
-					'name'      => __( 'Normal', 'simpleshop' ),
-					'shortName' => __( 'M', 'simpleshop' ),
-					'size'      => 22,
-					'slug'      => 'normal',
-				),
-				array(
-					'name'      => __( 'Large', 'simpleshop' ),
-					'shortName' => __( 'L', 'simpleshop' ),
-					'size'      => 36.5,
-					'slug'      => 'large',
-				),
-				array(
-					'name'      => __( 'Huge', 'simpleshop' ),
-					'shortName' => __( 'XL', 'simpleshop' ),
-					'size'      => 49.5,
-					'slug'      => 'huge',
-				),
-			)
-		);
-
-		// Editor color palette.
-		add_theme_support(
-			'editor-color-palette',
-			array(
-				array(
-					'name'  => __( 'Primary', 'simpleshop' ),
-					'slug'  => 'primary',
-					'color' => simpleshop_hsl_hex( 'default' === get_theme_mod( 'primary_color' ) ? 199 : get_theme_mod( 'primary_color_hue', 199 ), 100, 33 ),
-				),
-				array(
-					'name'  => __( 'Secondary', 'simpleshop' ),
-					'slug'  => 'secondary',
-					'color' => simpleshop_hsl_hex( 'default' === get_theme_mod( 'primary_color' ) ? 199 : get_theme_mod( 'primary_color_hue', 199 ), 100, 23 ),
-				),
-				array(
-					'name'  => __( 'Dark Gray', 'simpleshop' ),
-					'slug'  => 'dark-gray',
-					'color' => '#111',
-				),
-				array(
-					'name'  => __( 'Light Gray', 'simpleshop' ),
-					'slug'  => 'light-gray',
-					'color' => '#767676',
-				),
-				array(
-					'name'  => __( 'White', 'simpleshop' ),
-					'slug'  => 'white',
-					'color' => '#FFF',
-				),
-			)
-		);
-
-		// Add support for responsive embedded content.
-		add_theme_support( 'responsive-embeds' );
-	}
+        // Add theme support for selective refresh for widgets.
+        add_theme_support('customize-selective-refresh-widgets');        
+    }
 endif;
-add_action( 'after_setup_theme', 'simpleshop_setup' );
+add_action('after_setup_theme', 'simpleshop_setup');
+
+function simpleshop_add_editor_styles(){    
+    add_editor_style('customize-editor-style.css');
+}
+add_action('admin_init', 'simpleshop_add_editor_styles');
+
+
+function simpleshop_content_width(){
+    $GLOBALS['content_width'] = apply_filters('simpleshop_content_width', 1170);
+}
+add_action('after_setup_theme','simpleshop_content_width', 0);
+
+function simpleshop_widgets_init() {
+    register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'simpleshop' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'simpleshop' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		)
+	);
+}
+add_action('widgets_init', 'simpleshop_widgets_init');
+
+
+
+function simpleshop_assets() {
+    wp_enqueue_style('google-fonts','//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900',null,'1.0');
+    wp_enqueue_style('bootstrap-css',get_template_directory_uri().'/assets/vendor/bootstrap/css/bootstrap.min.css',null,'4.1.0');
+    wp_enqueue_style('font-awesome-css',get_template_directory_uri().'/assets/vendor/font-awesome/css/font-awesome.min.css',null,'4.7.0');
+    wp_enqueue_style('bicon-css',get_template_directory_uri().'/assets/vendor/bicon/css/bicon.css',null,'3.3.7');
+
+    wp_enqueue_style('woocommerce-layouts-css',get_template_directory_uri().'/assets/css/woocommerce-layouts.css',null,'1.0');
+    /* wp_enqueue_style('woocommerce-small-screen-css',get_template_directory_uri().'/assets/css/woocommerce-small-screen.css',null,'1.0');
+    wp_enqueue_style('woocommerce-css',get_template_directory_uri().'/assets/css/woocommerce.css',null,'1.0'); */
+
+    wp_enqueue_style('simpleshop-theme-css',get_template_directory_uri().'/assets/css/main.css',null,'1.0');
+    wp_enqueue_style('simpleshop-css',get_stylesheet_uri());
+
+
+
+
+    wp_enqueue_script("bootstrap-js",get_theme_file_uri("/assets/vendor/bootstrap/js/bootstrap.min.js"),array('jquery'),'4.1.0',true);
+    wp_enqueue_script("popper-js",get_theme_file_uri("/assets/vendor/popper.min.js"),array('jquery'),'3.3.7',true);
+    wp_enqueue_script("simpleshop-js",get_theme_file_uri("/assets/js/scripts.js"),array('jquery'),'1.0',true);
+
+
+
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment_reply');
+    }
+}
+add_action('wp_enqueue_scripts', 'simpleshop_assets');
+
+
